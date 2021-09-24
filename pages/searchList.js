a1ebb0ea3cf138e66218989f3f25c3807e88cfab
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-09-24 18:30:29 +0900

    print book list on jsx
    1. 검색 데이터 받아와서 페이지에 출력 테스트

--- pages/searchList.js
+++ pages/searchList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { Tabs } from 'antd';
@@ -7,23 +7,34 @@ import AppLayout from '../components/AppLayout';
 const SearchList = () => {
   const { TabPane } = Tabs;
 
+  const [books, setBooks] = useState([]);
+
   // 제목 URL에서 가져오기
   const router = useRouter();
   const link = router.query;
 
   const getBookList = () => {
+    let result = [];
     axios
-      .get(`http:localhost:4000/test`, {
+      .get(`http://localhost:4000/test`, {
         'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
       })
       .then(function (res) {
-        console.log(res);
+        result = res.data.response.docs;
+        console.log(result);
+        setBooks(result);
       })
       .catch(function (err) {
         console.log(err);
       });
-    return 1;
   };
+
+  const updateList = () => {
+    getBookList();
+    console.log(books);
+  };
+
+  const a = books.map((book) => <p>{book.doc.bookname}</p>);
 
   return (
     <AppLayout>
@@ -31,7 +42,8 @@ const SearchList = () => {
         <TabPane tab="검색" key="1">
           검색 결과입니다.
           <p>{link.title}</p>
-          <p>{getBookList()}</p>
+          <button onClick={updateList} value="버튼"></button>
+          {a}
         </TabPane>
         <TabPane tab="추천받기" key="2">
           책 추천 받기
