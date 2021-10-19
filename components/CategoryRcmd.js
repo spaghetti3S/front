e3b3403ee87160f6ad9447a1ec1d829b24415cf5
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-10-19 23:41:15 +0900

    Make category
    1. 카테고리 종류 추가

--- components/CategoryRcmd.js
+++ components/CategoryRcmd.js
@@ -2,30 +2,76 @@ import React from 'react';
 import { Tabs } from 'antd';
 
 import BookList from './BookList';
+import dtl from '../src/category';
 
 const { TabPane } = Tabs;
 
 const CategoryRcmd = () => {
+  const makeMenu = (title, key) => {
+    return (
+      <TabPane tab={title} key={key}>
+        <BookList code={key} />
+      </TabPane>
+    );
+  };
+
   return (
     <Tabs defaultActiveKey="1" centered>
-      <TabPane tab="문학" key="1">
-        <Tabs defaultActiveKey="1" centered>
-          <TabPane tab="아라비안" key="80">
-            <BookList page="0" code="80" />
-          </TabPane>
-          <TabPane tab="나이트" key="81">
-            <BookList page="0" code="81" />
-          </TabPane>
-          <TabPane tab="에서 춤을" key="82">
-            <BookList kdc="" page="0" code="82" />
-          </TabPane>
+      <TabPane tab="문학" key="80">
+        <Tabs defaultActiveKey="81" centered>
+          {dtl.literature.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
         </Tabs>
       </TabPane>
-      <TabPane tab="역사" key="2">
-        Content of Tab Pane 2
+      <TabPane tab="역사" key="90">
+        <Tabs defaultActiveKey="91" centered>
+          {dtl.history.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
       </TabPane>
-      <TabPane tab="언어" key="3">
-        Content of Tab Pane 3
+      <TabPane tab="철학" key="10">
+        <Tabs defaultActiveKey="11" centered>
+          {dtl.philosophy.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
+      </TabPane>
+      <TabPane tab="사회과학" key="30">
+        <Tabs defaultActiveKey="31" centered>
+          {dtl.socialScience.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
+      </TabPane>
+      <TabPane tab="자연과학" key="40">
+        <Tabs defaultActiveKey="41" centered>
+          {dtl.naturalScience.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
+      </TabPane>
+      <TabPane tab="기술과학" key="50">
+        <Tabs defaultActiveKey="51" centered>
+          {dtl.descriptiveScience.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
+      </TabPane>
+      <TabPane tab="예술" key="60">
+        <Tabs defaultActiveKey="61" centered>
+          {dtl.art.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
+      </TabPane>
+      <TabPane tab="언어" key="70">
+        <Tabs defaultActiveKey="71" centered>
+          {dtl.language.map((categoryInfo) => {
+            return makeMenu(categoryInfo[0], categoryInfo[1]);
+          })}
+        </Tabs>
       </TabPane>
     </Tabs>
   );
