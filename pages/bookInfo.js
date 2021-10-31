04138b151120c96a5f6cfc7dfaf371c4083f19fd
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-10-31 19:42:31 +0900

    Add book cover layout on info
    1. 책 표지 레이아웃 추가

--- pages/bookInfo.js
+++ pages/bookInfo.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 
 import AppLayout from '../components/AppLayout';
+import BookCoverLayout from '../components/BookCoverLayout';
 
 const BookInfo = () => {
-  return <AppLayout>책 정보</AppLayout>;
+  const [book, setBook] = useState({ coverLargeUrl: '' });
+
+  // 검색 키워드 URL 에서 가져옴
+  const router = useRouter();
+  const link = router.query;
+  // 책정보 api
+  const getBooksInfo = async (isbn) => {
+    await axios
+      .get(`http://localhost:4000/book/search/isbn?keyword=${isbn}`, {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      })
+      .then((res) => {
+        setBook(res.data.item[0]);
+      });
+  };
+
+  useEffect(() => {
+    getBooksInfo(link.isbn);
+  }, []);
+
+  return (
+    <AppLayout>
+      <BookCoverLayout imgLink={book.coverLargeUrl} />
+    </AppLayout>
+  );
 };
 
 export default BookInfo;
