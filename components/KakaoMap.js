8b7533d35a41d7c60f7a7c24520bfa8cb949b324
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-11-11 17:27:14 +0900

    Update get loan avail information
    1. 각 도서관마다 대출 가능 여부 가져오기

--- components/KakaoMap.js
+++ components/KakaoMap.js
@@ -1,29 +1,45 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
-import LibraryInfoLayout from './LibraryInfoLayout';
+import React, { useEffect } from 'react';
 
 const { kakao } = window;
 
 const KakaoMap = ({ isbn }) => {
-  const [libraryList, setLibraryList] = useState([]);
-
-  // 인포윈도우를 표시하는 클로저를 만드는 함수
-  const makeOverListener = (map, marker, infowindow) => {
-    return function () {
-      infowindow.open(map, marker);
-    };
-  };
-
-  // 인포윈도우를 닫는 클로저를 만드는 함수
-  const makeOutListener = (infowindow) => {
-    return () => {
-      infowindow.close();
-    };
+  // 인포 윈도우
+  const createInfowindow = async (lib, map, marker) => {
+    await axios
+      .get(`http://localhost:4000/book/loan/${lib.libCode}/${isbn}`, {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      })
+      .then((res) => {
+        let infowindow = '';
+        if (
+          res.data.result.hasBook === 'N' ||
+          res.data.result.loanAvailable === 'N'
+        ) {
+          infowindow = new kakao.maps.InfoWindow({
+            content: `<div class="infowindow">
+              <h4 id="name">${lib.libName}</h4>
+              대출 불가
+            </div>`,
+            removable: true,
+          });
+        } else {
+          infowindow = new kakao.maps.InfoWindow({
+            content: `<div class="infowindow">
+              <h4 id="name">${lib.libName}</h4>
+              대출 가능
+            </div>`,
+            removable: true,
+          });
+        }
+        kakao.maps.event.addListener(marker, 'click', () => {
+          infowindow.open(map, marker);
+        });
+      });
   };
 
   // 범위 내 도서관 정보 가져오기
-  const getLibraryMark = async (bounds, map, myPosition) => {
+  const getLibraryMark = async (bounds, map) => {
     await axios
       .post(`http://localhost:4000/library/around`, {
         'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
@@ -33,40 +49,36 @@ const KakaoMap = ({ isbn }) => {
         const libraryListInfo = res.data;
         // 마커 생성
         libraryListInfo.map((lib, index) => {
-          if (libraryListInfo[index] && !libraryListInfo[index].distance)
-            libraryListInfo[index].distance =
-              ((myPosition[0] - lib.latitude) ** 2 +
-                (myPosition[1] - lib.longitude) ** 2) **
-              0.5;
           const latlng = new kakao.maps.LatLng(lib.latitude, lib.longitude);
           const marker = new kakao.maps.Marker({
             map: map,
             position: latlng,
             title: lib.libName,
           });
-          const infowindow = new kakao.maps.InfoWindow({
-            content: lib.libName,
+          // kakao.maps.event.addListener(
+          //   marker,
+          //   'mouseover',
+          //   makeOverListener(map, marker, infowindow)
+          // );
+          // kakao.maps.event.addListener(
+          //   marker,
+          //   'mouseout',
+          //   makeOutListener(infowindow)
+          // );
+
+          createInfowindow(lib, map, marker);
+
+          const libraryOverlay = new kakao.maps.CustomOverlay({
+            position: latlng,
+            content: `<div class="infowindow">
+            <h4 id="name">${lib.libName}</h4>
+            </div>`,
           });
-          kakao.maps.event.addListener(
-            marker,
-            'mouseover',
-            makeOverListener(map, marker, infowindow)
-          );
-          kakao.maps.event.addListener(
-            marker,
-            'mouseout',
-            makeOutListener(infowindow)
-          );
         });
-        libraryListInfo.sort((a, b) => {
-          return a.distance - b.distance;
-        });
-        setLibraryList(libraryListInfo);
       });
   };
 
   useEffect(() => {
-    const myPosition = [37.365264512305174, 127.10676860117488];
     // 지도 띄우기
     const container = document.getElementById('map');
     const options = {
@@ -80,17 +92,13 @@ const KakaoMap = ({ isbn }) => {
     map.setCenter(moveLatLon);
     // 지도 이동 이벤트
     kakao.maps.event.addListener(map, 'dragend', () => {
-      getLibraryMark(map.getBounds().toString(), map, myPosition);
+      getLibraryMark(map.getBounds().toString(), map);
     });
 
     // 지도 확대/축소 끄기
     map.setZoomable(false);
-    // 지도 확대/축소 이벤트
-    // kakao.maps.event.addListener(map, 'zoom_changed', () => {
-    //   getLibraryMark(map.getBounds().toString(), map, myPosition);
-    // });
 
-    getLibraryMark(map.getBounds().toString(), map, myPosition);
+    getLibraryMark(map.getBounds().toString(), map);
 
     // 위치 정보를 불러와서 지도이동
     // if (navigator.geolocation) {
@@ -104,14 +112,15 @@ const KakaoMap = ({ isbn }) => {
     // } else {
     //   console.log('위치정보를 불러올 수 없습니다.');
     // }
-  }, []);
+  }, [isbn]);
   return (
-    <div id="libraryLayout">
-      <div id="map" style={{ width: '60%', height: '500px' }}>
-        카카오맵
+    <>
+      <div id="libraryLayout">
+        <div id="map" style={{ width: '95%', height: '500px' }}>
+          카카오맵
+        </div>
       </div>
-      <LibraryInfoLayout library={libraryList} isbn={isbn} />
-    </div>
+    </>
   );
 };
 
