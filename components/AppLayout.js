2318e4f11bc14a84214da95e9570ec0be418d7f6
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-11-16 21:58:06 +0900

    Create user function
    1. 회원가입 구현
    2. 로그인 구현
    3. 로그아웃 구현

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -2,17 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Layout } from 'antd';
+import axios from 'axios';
+import Router from 'next/router';
+
 import SearchBar from './SearchBar';
 
 const { Header, Footer, Content } = Layout;
 
 const AppLayout = ({ children }) => {
+  const logoutReq = async () => {
+    await axios
+      .get(`http://localhost:4000/user/logout`, {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      })
+      .then((res, err) => {
+        if (!err || res.data.logoutSuccess) {
+          window.localStorage.removeItem('userId');
+          alert('로그아웃 되었습니다. ');
+          Router.push('/');
+        }
+      });
+  };
   return (
     <Layout>
       <Header>
         <Link href="/">
           <a href="{() => false}">홈이동</a>
         </Link>
+        {window.localStorage.getItem('userId') && (
+          <a onClick={logoutReq} style={{ marginLeft: '20px' }}>
+            로그아웃
+          </a>
+        )}
+        {!window.localStorage.getItem('userId') && (
+          <Link href="/login">
+            <a style={{ margin: '20px' }} href="{() => false}">
+              로그인
+            </a>
+          </Link>
+        )}
       </Header>
       <Content style={{ backgroundColor: 'white' }}>
         <SearchBar />
---
+++ components/RegisterForm.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Form, Input, Radio, InputNumber, Button } from 'antd';
+import Router from 'next/router';
+import axios from 'axios';
+
+const RegisterForm = () => {
+  const registerReq = async (val) => {
+    if (val.password !== val.password_check) {
+      alert('비밀번호가 일치하지 않습니다.');
+    } else {
+      await axios
+        .post(`http://localhost:4000/user/register`, {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+          info: val,
+        })
+        .then((res, err) => {
+          if (err) {
+            alert('회원 가입에 실패했습니다.');
+          } else {
+            if (!res.data.registerSuccess) {
+              alert('이미 존재하는 아이디 입니다.');
+            } else {
+              alert('회원가입이 완료되었습니다.\n로그인 해주세요.');
+              Router.push('/login');
+            }
+          }
+        });
+    }
+  };
+
+  return (
+    <div style={{ margin: '20%' }}>
+      <Form
+        onFinish={registerReq}
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 14 }}
+      >
+        <Form.Item
+          label="아이디"
+          name="id"
+          rules={[{ required: true, message: '아이디를 입력하세요.' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="비밀번호"
+          name="password"
+          rules={[{ required: true, message: '비밀번호를 입력하세요.' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="비밀번호 확인"
+          name="password_check"
+          rules={[{ required: true, message: '비밀번호를 입력하세요.' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          name="age"
+          label="나이"
+          rules={[
+            {
+              type: 'number',
+              min: 0,
+              max: 99,
+              required: true,
+              message: '정확한 나이를 입력하세요.',
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="sex"
+          label="성별"
+          rules={[{ required: true, message: '성별을 선택하세요' }]}
+        >
+          <Radio.Group>
+            <Radio.Button value="M">남</Radio.Button>
+            <Radio.Button value="F">여</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 6 }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            회원가입
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterForm;
