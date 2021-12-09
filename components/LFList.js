00c409c025bfdeb5a7412e9951d5ed2d72ce137c
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2021-12-09 21:45:35 +0900

    Fix list.js
    1. list 페이지 수정
    2. lint 삭제

---
+++ components/LFList.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+
+import WishComponent from './WishComponent';
+
+const LFList = () => {
+  const [wishList, setWishList] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const router = useRouter();
+  const link = router.query;
+
+  const getList = async () => {
+    if (link.type) {
+      await axios
+        .post(`http://localhost:4000/user/book/`, {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+          token: window.localStorage.getItem('token'),
+          state: link.type,
+        })
+        .then((res) => {
+          setWishList(res.data.data);
+        });
+    }
+  };
+
+  const nextPage = () => {
+    const totalPage =
+      wishList.length % 12 === 0
+        ? parseInt(wishList.length / 12)
+        : parseInt(wishList.length / 12) + 1;
+    if (totalPage - 1 > page) {
+      setPage(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page !== 0) {
+      setPage(page - 1);
+    }
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+  return (
+    <div>
+      <div id="mypage">
+        <div id="pageButton" onClick={prevPage}>
+          <LeftOutlined />
+        </div>
+        <div id="wishWrapper">
+          {wishList.slice(page * 12, page * 12 + 12).map((book) => {
+            return <WishComponent isbn={book.isbn} />;
+          })}
+        </div>
+        <div id="pageButton" onClick={nextPage}>
+          <RightOutlined />
+        </div>
+      </div>
+      <p style={{ textAlign: 'center' }}>
+        {page + 1} /{' '}
+        {wishList.length % 12 === 0
+          ? parseInt(wishList.length / 12)
+          : parseInt(wishList.length / 12) + 1}
+      </p>
+    </div>
+  );
+};
+
+export default LFList;
